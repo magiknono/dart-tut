7b1a1c0c5d1007c759a64950129cecf504df64e6
arnaud <acormier@upyourbizz.fr>
2019-07-11 17:16:15 +0200

    variables in dart

---
+++ hello_var.dart
@@ -0,0 +1,19 @@
+void main() {
+  // implicit var
+  var a = 1;
+  print(a);
+
+  // explicit var with type
+  int b = 2;
+  print(b);
+
+  // constant var (cannot be change)
+  final c = "hello";
+  // c = "hello"; // = ERROR
+  print(c);
+
+  // constant var with keyword
+  const d = "World"; // Number, STring, Boolean, Array, Map, Symbol
+  print(d);
+
+}
